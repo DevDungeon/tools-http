9836dd8ce3b514a444f2b06fb200d7e9a4c5294c
NanoDano <nanodano@devdungeon.com>
2018-01-15 21:46:38 -0600

    Adding project files for testing with Maven central deploy

---
+++ src/main/java/com/devdungeon/tools/http/Client.java
@@ -0,0 +1,7 @@
+package com.devdungeon.tools.http;
+
+public class Client {
+    public static void downloadURL(String url, String filepath) {
+        System.out.println("Downloading " + url + " to " + filepath);
+    }
+}
