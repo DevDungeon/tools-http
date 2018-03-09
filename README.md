4d6d6948ab338df2f00e8fdc763fab751e40d8c8
NanoDano <nanodano@devdungeon.com>
2018-03-08 18:48:50 -0600

    Latest tweaks

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tools-http
 Java utility library for HTTP
+
+[![Maven metadata URI](https://img.shields.io/maven-metadata/v/http/central.maven.org/maven2/com/devdungeon/tools/http/maven-metadata.xml.svg)]()
--- src/main/java/com/devdungeon/tools/http/Client.java
+++ src/main/java/com/devdungeon/tools/http/Client.java
@@ -1,14 +1,11 @@
 package com.devdungeon.tools.http;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 import java.io.*;
-
 
 /**
  * @author nanodano@devdungeon.com
@@ -16,42 +13,31 @@ import java.io.*;
 public class Client {
 
     public static void main(String[] args) {
-        System.out.println("Not implemented.");
+        try {
+            downloadURL("https://www.devdungeon.com", "test.html");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void downloadURL(String url, String filepath) {
-        System.out.println("Downloading " + url + " to " + filepath);
+    public static void downloadURL(String url, String filepath) throws IOException {
 
-        HttpClient client = new DefaultHttpClient();
-
-        HttpParams params = client.getParams();
-        HttpConnectionParams.setConnectionTimeout(params, 1000 * 5);
-        HttpConnectionParams.setSoTimeout(params, 1000 * 5);
-
+        CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpGet request = new HttpGet(url);
+        HttpResponse response = httpClient.execute(request);
 
-        HttpResponse response = null;
-        try {
-            response = client.execute(request);
-        } catch (IOException ex) {
-            // Ignore
+        BufferedReader reader;
+        String line;
+        reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        FileWriter fileWriter = new FileWriter(filepath);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        while ((line = reader.readLine()) != null) {
+            bufferedWriter.write(line);
+            bufferedWriter.newLine();
         }
-        BufferedReader rd;
-        String line = null;
-        try {
-            rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-            FileWriter fileWriter = new FileWriter(filepath);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            while ((line = rd.readLine()) != null) {
-                bufferedWriter.write(line);
-                bufferedWriter.newLine();
-            }
-            bufferedWriter.close();
-
-        } catch (IOException ex) {
-            // Ignore
-        } catch (UnsupportedOperationException ex) {
-            // Ignore
-        }
+        bufferedWriter.close();
     }
+
+
+
 }
